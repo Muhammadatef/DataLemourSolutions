278227a8506378528dff6c12d5ff797dcff57ac2
Mohamed Atef Fahmy <muhamedfahmy74@yahoo.com>
2024-04-13 10:19:09 +0200

    Update Histogram of Tweets.sql

--- Easy/Histogram of Tweets.sql
+++ Easy/Histogram of Tweets.sql
@@ -28,15 +28,18 @@ tweet_bucket	users_num
 
 SOLUTION: 
   
+WITH total_tweets AS (
+  SELECT 
+    user_id, 
+    COUNT(tweet_id) AS tweet_count_per_user
+  FROM tweets 
+  WHERE tweet_date BETWEEN '2022-01-01' 
+    AND '2022-12-31' 
+  GROUP BY user_id) 
+  
 SELECT 
-  user_id, 
-  COUNT(tweet_id) AS tweet_count_per_user 
-FROM tweets 
-WHERE tweet_date BETWEEN '2022-01-01' 
-  AND '2022-12-31'
-GROUP BY user_id;
-The output shows the number of tweets posted by each user in 2022:
-user_id	tweet_count_per_user
-111	2
-148	1
-254	1
+  tweet_count_per_user AS tweet_bucket, 
+  COUNT(user_id) AS users_num 
+FROM total_tweets 
+GROUP BY tweet_count_per_user;
+
